7ce59e7882208bd26f4e40d1198538c8c3fdb4c3
Fatima Sohail <23100065@lums.edu.pk>
2020-08-05 18:01:05 +0500

    Assignment Soultion for Module 4

---
+++ module_4/js/script.js
@@ -0,0 +1,17 @@
+
+(function(window){
+ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i=0; i< names.length; i++) {
+
+  var name = names[i];
+  var firstLetter = name.charAt(0);
+  firstLetter=firstLetter.toLowerCase();
+
+  if (firstLetter==='j') {
+    byeSpeaker.speak(name);
+  } else {
+    helloSpeaker.speak(name);
+  }
+}
+})(window);
